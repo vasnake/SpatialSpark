a97a8d086d3b73648d36ee873f56d8aba172934f
Fedulov, Valentin <vasnake@gmail.com>
2019-04-15 20:50:46 +0300

    refactored test

--- src/test/scala/spatialspark/join/BroadcastSpatialJoinTest.scala
+++ src/test/scala/spatialspark/join/BroadcastSpatialJoinTest.scala
@@ -4,18 +4,29 @@ import org.scalatest._
 import com.holdenkarau.spark.testing.SharedSparkContext
 
 import org.apache.spark.rdd.RDD
-import org.locationtech.jts.geom.{Coordinate, Geometry, GeometryFactory, PrecisionModel}
 
 import spatialspark.operator.SpatialOperator
-import util.StringToolbox._
 
 class BroadcastSpatialJoinTest extends
   FlatSpec with Matchers with SharedSparkContext {
 
   import spatialspark.join.BroadcastSpatialJoinTest._
 
+  private def BSJ(
+    left: RDD[DataType],
+    right: RDD[DataType],
+    op: SpatialOperator.SpatialOperator,
+    radius: Double = 0d,
+    condition: Option[ConditionType] = None
+  ): Seq[ResultNoGeomType] = {
+
+    BroadcastSpatialJoin(sc, left, right, op, radius, condition)
+      .map{ case (ls, rs, _, _) => (ls, rs) }
+      .collect
+  }
+
   // testOnly spatialspark.join.BroadcastSpatialJoinTest -- -z "contains"
-  it should "area contains points" in {
+  it should "find polygon that contains point" in {
     // input data
     val leftTab =
       """
@@ -35,25 +46,21 @@ class BroadcastSpatialJoinTest extends
     val right: RDD[DataType] = sc.parallelize(parsePoints(rightTab))
 
     // join params
-    val op = SpatialOperator.Contains // Within, WithinD, Contains, Intersects, Overlaps, NearestD
+    val op = SpatialOperator.Contains
     val radius = 0d // 120 * 1000d // 120 km
-    val condition: Option[ConditionType] = Some((ls: String, rs: String) =>
-      ls.toLowerCase == rs.toLowerCase)
+    val condition: Option[ConditionType] = Some(
+      (ls: String, rs: String) => ls.toLowerCase == rs.toLowerCase
+    )
 
     // expected
-    val expectedTab =
+    val expected =
       """
         |a, A
         |b, B
       """.stripMargin
 
-    val expected: ResultNoGeomType = parsePairs(expectedTab)
-
     // test
-    val output = BroadcastSpatialJoin(sc, left, right, op, radius, condition)
-
-    output.map{ case (ls, rs, _, _) => (ls, rs) }
-      .collect should contain theSameElementsAs expected
+    BSJ(left, right, op, radius, condition) should contain theSameElementsAs parsePairs(expected)
 
     def noExtraCondition() = {
       val expected =
@@ -64,45 +71,29 @@ class BroadcastSpatialJoinTest extends
           |bb, B
         """.stripMargin
 
-      BroadcastSpatialJoin(sc, left, right, op, radius, None)
-        .map{ case (ls, rs, _, _) => (ls, rs) }
-        .collect should contain theSameElementsAs parsePairs(expected)
+      BSJ(left, right, op, radius) should contain theSameElementsAs parsePairs(expected)
     }
 
     noExtraCondition()
   }
+
+  // TODO: Within, WithinD, Intersects, Overlaps, NearestD
 }
 
 object BroadcastSpatialJoinTest {
+  import org.locationtech.jts.geom.{Coordinate, Geometry, GeometryFactory, PrecisionModel}
+  import util.StringToolbox._
+  import scala.language.implicitConversions
 
   type DataType = (String, Geometry)
   type ConditionType = (String, String) => Boolean
-  type ResultNoGeomType = Seq[(String, String)]
-
-  val sridWGS84 = 4326
-  val gf = new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), sridWGS84)
-
-  def polygon(points: Array[String]): Geometry = {
-    val xys = for {
-      Array(lon, lat) <- points.map(_.splitTrim(Separators(",")))
-    } yield (lon, lat)
-
-    val coords: Array[Coordinate] = (xys :+ xys.head) map { case (x, y) =>
-      new Coordinate(x.toDouble, y.toDouble) }
-
-    gf.createPolygon(coords).asInstanceOf[Geometry]
-  }
-
-  def point(xy: Array[String]): Geometry = xy match {
-    case Array(lon, lat) => gf.createPoint(new Coordinate(lon.toDouble, lat.toDouble))
-    case xz => throw new IllegalArgumentException(s"xy must be Array(x, y); got `$xy` instead")
-  }
+  type ResultNoGeomType = (String, String)
 
   def parsePolygons(str: String): Seq[DataType] = {
     // one line sample: `a:  1,1; 2,1; 2,2`
     val seq: Seq[(String, Geometry)] = for {
-      Array(key, points) <- str.splitTrim(Separators("\n")).map(_.splitTrim(Separators(":")))
-    } yield (key, polygon(points.splitTrim(Separators(";"))))
+      Array(key, points) <- pairs(str)
+    } yield (key, polygon(points.splitTrim(";")))
     //println(s"parsePolygon: \n\t${seq.mkString("\n\t")}")
     seq
   }
@@ -111,21 +102,44 @@ object BroadcastSpatialJoinTest {
     // one line sample: `A: 1.5, 1.3`
     val seq: Seq[(String, Geometry)] = {
       for {
-        Array(key, xy) <- str.splitTrim(Separators("\n")).map(_.splitTrim(Separators(":")))
-      } yield (key, point(xy.splitTrim(Separators(","))))
+        Array(key, xy) <- pairs(str)
+      } yield (key, point(xy.splitTrim(",")))
     }
     //println(s"parsePoints: \n\t${seq.mkString("\n\t")}")
     seq
   }
 
-  def parsePairs(str: String): ResultNoGeomType = {
+  def parsePairs(str: String): Seq[ResultNoGeomType] = {
     // one line sample: `a, A`
     val seq: Seq[(String, String)] = for {
-      Array(a, b) <- str.splitTrim(Separators("\n")).map(_.splitTrim(Separators(",")))
+      Array(a, b) <- pairs(str, ",")
     } yield (a, b)
     //println(s"parsePairs: \n\t${seq.mkString("\n\t")}")
     seq
   }
+
+  val sridWGS84 = 4326
+  val gf = new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), sridWGS84)
+
+  implicit private def stringToSeparator(sep: String): Separators = Separators(sep)
+
+  private def polygon(points: Array[String]): Geometry = {
+    val xys = for {
+      Array(lon, lat) <- points.map(_.splitTrim(","))
+    } yield (lon, lat)
+
+    val coords: Array[Coordinate] = (xys :+ xys.head) map { case (x, y) =>
+      new Coordinate(x.toDouble, y.toDouble) }
+
+    gf.createPolygon(coords).asInstanceOf[Geometry]
+  }
+
+  private def point(xy: Array[String]): Geometry = xy match {
+    case Array(lon, lat) => gf.createPoint(new Coordinate(lon.toDouble, lat.toDouble))
+    case _ => throw new IllegalArgumentException(s"xy must be Array(x, y); got `$xy` instead")
+  }
+
+  private def pairs(str: String, sep: String = ":") = str.splitTrim("\n").map(_.splitTrim(sep))
 
   // check contains
   def check(left: RDD[DataType], right: RDD[DataType]): Unit = for {
